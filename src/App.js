0050ddf95c000ca354e674ab6a3dfcefe3f19185
Victor Alves da Costa <victoralves741@gmail.com>
2022-04-08 14:05:35 -0300

    Primeiro commit com do projeto de curriculo/repositorio de informações pessoas destinado a entrevista de empregos

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+
+import './App.css';
+
+function App() {
+  const str = ('Olá mundo');
+
+  const [InputData, setInputData] = useState() 
+
+  function handleonClick(){
+    const data = document.getElementById("InputData")
+    setInputData(data.value)
+    }
+
+  return (
+    <div className="App">
+        <h2>{str} {InputData}</h2>
+        
+        <input placeholder='Digite seu nome' id="InputData" type={'text'}></input>
+
+            <button onClick={handleonClick}>
+                Clique aqui
+            </button>
+        
+    </div>
+  );
+}
+export default App;
