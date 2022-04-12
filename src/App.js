b0a5a61c0fe6da9215d34725fc57cc97cc9a9951
Victor Alves da Costa <victoralves741@gmail.com>
2022-04-12 18:26:51 -0300

    1.2

--- src/App.js
+++ src/App.js
@@ -13,16 +13,17 @@ function App() {
     }
 
   return (
-    <div className="App">
-        <h2>{str} {InputData}</h2>
-        
-        <input placeholder='Digite seu nome' id="InputData" type={'text'}></input>
-
-            <button onClick={handleonClick}>
-                Clique aqui
-            </button>
-        
+    <div>
+          <div className='Interno'>
+            <h2>{str} {InputData}</h2>
+      
+            <input placeholder='Digite seu nome' id="InputData" type={'text'}></input>
+                <button onClick={handleonClick}>
+                    Clique aqui
+                </button>
+          </div>
     </div>
+    
   );
 }
 export default App;
